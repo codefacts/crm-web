0ae58575d0cf20f6850866ab971511619c2045ec
sohan <sohan09014@gmail.com>
2015-10-12 01:52:15 +0600

    br_checker_app jar file creating with maven-assembly-plugin

--- src/main/java/io/crm/web/App.java
+++ src/main/java/io/crm/web/App.java
@@ -3,10 +3,12 @@ package io.crm.web;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
-import java.io.*;
-
-import static org.apache.commons.io.FileUtils.readFileToString;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by someone on 07/09/2015.
@@ -17,35 +19,26 @@ public class App {
     public static final String STATIC_DIRECTORY = App.loadConfig().getString("STATIC_DIRECTORY");
     public static final String PUBLIC_DIRECTORY = App.loadConfig().getString("PUBLIC_DIRECTORY");
 
-    public static JsonObject loadConfig() {
-        if (config == null) {
-            final File file = new File(CONFIG_FILE_NAME);
-            if (file.exists()) {
-                try {
-                    return new JsonObject(FileUtils.readFileToString(file));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            final InputStream inputStream = App.class.getClassLoader().getResourceAsStream(CONFIG_FILE_NAME);
-            final InputStreamReader reader = new InputStreamReader(inputStream);
-            final StringBuilder builder = new StringBuilder();
-            final char[] chars = new char[1024];
-            int read;
-            try {
-                while ((read = reader.read(chars, 0, chars.length)) > 0) {
-                    builder.append(chars, 0, read);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            return new JsonObject(builder.toString());
-        } else return config;
+    static {
+        if (loadConfig().getBoolean("dev-mode")) {
+            System.setProperty("dev-mode", "true");
+        }
     }
 
-//    static {
-//        System.setProperty("dev-mode", "true");
-//    }
+    public static JsonObject loadConfig() {
+        try {
+            if (config == null) {
+                final File file = new File(CONFIG_FILE_NAME);
+                if (file.exists()) {
+                    return new JsonObject(FileUtils.readFileToString(file));
+                }
+                final InputStream inputStream = App.class.getClassLoader().getResourceAsStream(CONFIG_FILE_NAME);
+                return new JsonObject(IOUtils.toString(inputStream));
+            } else return config;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String... args) {
         Vertx.vertx().deployVerticle(new MainVerticle());
