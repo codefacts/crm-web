0cdf7c9e19922cf3d21a95c35791b4b2114c621d
codefacts <codefacts63@gmail.com>
2015-11-09 12:42:09 +0600

    All Tuples COmplete

--- src/main/java/io/crm/web/controller/FileUploadController.java
+++ src/main/java/io/crm/web/controller/FileUploadController.java
@@ -3,7 +3,6 @@ package io.crm.web.controller;
 import com.google.common.collect.ImmutableList;
 import io.crm.FailureCode;
 import io.crm.util.*;
-import io.crm.util.touple.MutableTpl2;
 import io.crm.web.ApiEvents;
 import io.crm.web.ST;
 import io.crm.web.Uris;
